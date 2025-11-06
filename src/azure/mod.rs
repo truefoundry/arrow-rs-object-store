4a934c0acfe5d488f5b908681d7dd8e40c5cce5f
Chirag Jain <jain.chirag925@gmail.com>
2025-11-06 13:54:42 +0530

    Use startFrom for list with offset (#1)
    * Use startFrom for list with offset
    
    * Add more tracing

--- src/azure/mod.rs
+++ src/azure/mod.rs
@@ -123,6 +123,7 @@ impl ObjectStore for MicrosoftAzure {
     fn list(&self, prefix: Option<&Path>) -> BoxStream<'static, Result<ObjectMeta>> {
         self.client.list(prefix)
     }
+
     fn delete_stream<'a>(
         &'a self,
         locations: BoxStream<'a, Result<Path>>,
@@ -153,6 +154,27 @@ impl ObjectStore for MicrosoftAzure {
 
     async fn copy_if_not_exists(&self, from: &Path, to: &Path) -> Result<()> {
         self.client.copy_request(from, to, false).await
+    }
+
+    fn list_with_offset(
+        &self,
+        prefix: Option<&Path>,
+        offset: &Path,
+    ) -> BoxStream<'static, Result<ObjectMeta>> {
+        use std::env;
+        match env::var("AZURE_BLOB_USE_START_FROM").as_deref() {
+            Ok("true") => {
+                tracing::info!("azure::client::list_with_offset");
+                self.client.list_with_offset(prefix, offset)
+            }
+            _ => {
+                let offset = offset.clone();
+                tracing::info!("azure::client::list + filter");
+                self.list(prefix)
+                    .try_filter(move |f| futures::future::ready(f.location > offset))
+                    .boxed()
+            }
+        }
     }
 }
 
